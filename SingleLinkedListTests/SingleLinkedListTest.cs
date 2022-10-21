f408f83290606237a986013430ff06487da37f6a
Martin Mueller <martin.ernst.mueller@gmail.com>
2022-10-21 13:27:53 +0200

    Add SwitchNodes

--- SingleLinkedListTests/SingleLinkedListTest.cs
+++ SingleLinkedListTests/SingleLinkedListTest.cs
@@ -11,6 +11,10 @@ namespace TestProject1
     public class SingleLinkedListTest
     {
         SingleLinkedList mySLL = new SingleLinkedList();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
         [SetUp]
         public void Setup()
         {
@@ -21,11 +25,26 @@ namespace TestProject1
         public void TestSLLCount_AddItems_ReturnsCorrectCount()
         {
             var myLL = new SingleLinkedList();
-            myLL.insert(new Node(1));
-            myLL.insert(new Node(1));
-            myLL.insert(new Node(1));
-            myLL.insert(new Node(1));  
+            myLL.InsertLast(new Node(1));
+            myLL.InsertLast(new Node(1));
+            myLL.InsertLast(new Node(1));
+            myLL.InsertLast(new Node(1));  
             Assert.AreEqual(4, myLL.count());
+        }
+
+        [Test]
+        public void TestSLLCount_SwitchExistingNodes_ReturnsTrue()
+        {
+            var myLL = new SingleLinkedList();
+            
+            myLL.InsertLast(node1);
+            myLL.InsertLast(node2);
+            myLL.InsertLast(node3);
+            myLL.InsertLast(node4);
+            Assert.AreEqual(4, myLL.count());
+            myLL.SwitchNode(new Node(2), new Node(3));
+            Assert.AreEqual(4, myLL.count());
+
         }
 
         [Test]
