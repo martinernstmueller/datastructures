46b7dfd5c62181851e70c7175178fab4437699e3
Martin Mueller <martin.ernst.mueller@gmail.com>
2022-10-21 13:36:38 +0200

    Add 1 more Unit test

--- SingleLinkedListTests/SingleLinkedListTest.cs
+++ SingleLinkedListTests/SingleLinkedListTest.cs
@@ -78,5 +78,20 @@ namespace TestProject1
 
         }
 
+        [Test]
+        public void TestSLLSwitchNodes_NoNodeDoesExist_DontChangeAnything()
+        {
+            var myLL = new SingleLinkedList();
+
+            myLL.InsertLast(node1);
+            myLL.InsertLast(node2);
+            myLL.InsertLast(node3);
+            myLL.InsertLast(node4);
+            Assert.AreEqual(4, myLL.count());
+            myLL.SwitchNode(new Node(42), new Node(24));
+            Assert.AreEqual(myLL.ToString(), "| 1 | 2 | 3 | 4 |");
+
+        }
+
     }
 }
