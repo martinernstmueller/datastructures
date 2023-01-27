151378df596ea3b4e2f74eccbe2da33ccd0ef304
Martin Mueller <martin.ernst.mueller@gmail.com>
2023-01-27 13:47:55 +0100

    Add Binary Tree

---
+++ Common/BinaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class BinaryTreeNode
+    {
+        public int data;
+        public BinaryTreeNode LeftChild;
+        public BinaryTreeNode RightChild;
+        // Constructor to create a new node
+        public BinaryTreeNode(int argData) { data = argData; }
+        public bool IsLeaf()
+        {
+            return LeftChild == null && RightChild == null;
+        }
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+    }
+
+}
